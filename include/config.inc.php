987d008b3751c95561d93a6864e14d3a889bbe8d
Ross Paskett <ross.paskett@raremotion.com>
2011-03-22 22:12:27 -0600

    Adding global config file to move unchangeable settings out of app config. Makes app config cleaner, too.

---
+++ include/config.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+define( 'PATH_LIB_CLASS',		PATH_LIB . '/classes' );
+define( 'PATH_LIB_INCLUDE',		PATH_LIB . '/include' );
+
+$__files['classes'] = array_merge( array(
+		'message'				=>	PATH_LIB_CLASS . '/message.class.php',
+		'timer'					=>	PATH_LIB_CLASS . '/timer.class.php',
+		'mysql'					=>	PATH_LIB_CLASS . '/mysql.class.php',
+		'query'					=>	PATH_LIB_CLASS . '/query.class.php',
+		'page'					=>	PATH_LIB_CLASS . '/page.class.php',
+		'pmail'					=>	PATH_LIB_CLASS . '/pmail.class.php',
+		'database'				=>	PATH_LIB_CLASS . '/database.abstract.php',
+		'Domain\Model'			=>	PATH_LIB_CLASS . '/model.domain.php',
+		'Domain\Meta'			=>	PATH_LIB_CLASS . '/meta.domain.php',
+		'Service\Model'			=>	PATH_LIB_CLASS . '/model.service.php',
+), $__files['classes'] );
+
+?>
