b38d80b62e09b1a81156300919da889f55baef6d
Ross Paskett <ross@paskett.me>
2011-04-14 18:27:05 -0600

    Simplified error handler passing to exception handler.

--- include/include.inc.php
+++ include/include.inc.php
@@ -3,16 +3,21 @@
  * @author ross paskett <rpaskett@gmail.com>
  */
 
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-}
+/*function exception_error_handler( $errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException ($errstr, 0, $errno, $errfile, $errline );
+}*/
 
-set_error_handler('exception_error_handler');
+// this passes script errors to the exception handler
+set_error_handler(function( $errno, $errstr, $errfile, $errline )
+{
+	throw new ErrorException( $errstr, 0, $errno, $errfile, $errline );
+});
 
-date_default_timezone_set('America/Denver');
+date_default_timezone_set( 'America/Denver' );
 
 class config{}
 $config = new config;
+
 
 // these files are required, they include some
 // essential constants and some setup functions
@@ -21,19 +26,24 @@ require( 'config.inc.php' );
 include( 'http_status.inc.php' );
 include( 'functions.inc.php' );
 
-// this function must be before session starting. sometimes objects get passed
-// as session vars, so the system needs to know how to create them before the
-// session tries to create an instance (it does as soon as the session loads)
 
+// this will attempt to autoload a class, otherwise
+// it throws an exception and halts execution
 function __autoload( $file )
 {
 	$part = explode( '\\', $file );
 	global $config;
 	$sub = ( empty( $part[1] ) ? 'class' : $part[0] );
-	//echo $sub;
-	if( is_file( $config->classes[$file] ) )
+	try
+	{
+		if( !is_file( $config->classes[$file] ) )
+			throw new Exception( "Unable to load class: $file" );
+
 		require_once( $config->classes[$file] );
-	else
-		throw new Exception( "Unable to load class: $file" );
+	}
+	catch( Exception $e )
+	{
+		die( $e->getMessage() );
+	}
 }
 ?>
