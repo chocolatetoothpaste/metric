38a4bff89eb9d152f0fc8cd0c826b327a701382f
Ross Paskett <rpaskett@gmail.com>
2011-06-01 15:11:13 -0600

    Problem with request class, removing content-length for now.  Added xml type to rest server, very infantile implementation.

--- class/request.class.php
+++ class/request.class.php
@@ -13,7 +13,7 @@ class request
 	public function exec()
 	{
 		if( is_array( $this->content ) )
-		$this->content = http_build_query( $this->content );
+			$this->content = http_build_query( $this->content );
 
 		$this->hash();
 		$this->auth = base64_encode( "{$this->username}:{$this->hash}" );
@@ -38,7 +38,11 @@ class request
  			if( $this->method === 'GET' )
 				$this->url .= "?{$this->content}";
 			else
+			{
 				$options[CURLOPT_POSTFIELDS] = $this->content;
+				//$this->length = strlen( $this->content );
+				$headers[] = "Content-Length: {$this->content}";
+			}
 		}
 
 		$options[CURLOPT_URL] = $this->host . $this->url;
@@ -56,6 +60,7 @@ class request
 		
 		$this->hash = "{$this->method} HTTP/1.1 {$this->url}\n"
 			. "Date: {$this->date}\n\n"
+			//. "Content-Length: {$this->length}\n\n"
 			. "{$this->content}";
 		$this->hash = hash_hmac( 'sha1', $this->hash, $this->key );
 	}
