10538edff1c75378f1500b622fe4275ad6ffb2ab
Ross Paskett <rpaskett@gmail.com>
2011-12-20 17:01:10 -0700

    Abstracting REST authentication into a user defined method (called authorize for now, but will change when I think of something proper an less ambiguous)

--- rest.php
+++ rest.php
@@ -1,45 +1,12 @@
 <?php
 $page->template = false;
+$page->authorize();
 $page->content_type = $_SERVER['HTTP_ACCEPT'];
-$response = array(
+$page->response = array(
 	'success' => 'false',
 	'status' => $config->HTTP_UNAUTHORIZED,
 	'message' => 'You do not have permission to access the resource at '
 		. $page->request );
-
-// Authorization header is hidden from PHP's
-// $_SERVER super global, so grab it from apache
-$auth = apache_request_headers();
-
-//error_log(print_r($_SERVER, true));
-
-// Make sure credentials were passed, otherwise
-// there's no point in going any further
-if( empty( $auth['Authorization'] ) )
-{
-	header( $__http_status[$response['status']] );
-	header( 'WWW-Authenticate: Signature realm="Calendar"' );
-	die( json_encode( $response ) );
-}
-
-// Reset value of $auth to decoded Authorization
-// header and extract $username and signed message hash
-$auth = base64_decode( $auth['Authorization'] );
-list( $username, $signature ) = explode( ':', $auth );
-
-//~ error_log($signature);
-
-$db = mysql::instance( $config->db[$config->DB_MAIN] );
-$query = "
-	SELECT
-		api_key
-	FROM
-		users
-	WHERE
-		enabled = 1 AND username = :username";
-
-$db->execute( $query, array( 'username' => $username ) );
-$key = $db->result->fetchColumn();
 
 if( $_SERVER['REQUEST_METHOD'] === 'GET' )
 {
@@ -52,41 +19,10 @@ else
 	parse_str( $input, $data );
 }
 
-// Grab the length of the actual request to make sure it hasn't been modified
-// in transit. This is safer than relying on the Content-Length header since
-// it could be spoofed. It won't matter though, if the request wasn't modified,
-// the lenghts will match and the signature will match
-$length = strlen( $input );
-
-// This will change how a message is hashed, so any changes to this composition
-// could potentially break code. see also request.class.php; changes to this
-// composition must also be reflected in that class!!!
-$doc = <<<EODOC
-{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']} {$_SERVER['SERVER_PROTOCOL']}
-Date: {$_SERVER['HTTP_DATE']}
-Content-Length: {$length}
-
-$input
-EODOC;
-
-$hash = hash_hmac( 'sha1', utf8_encode( $doc ), $key );
-
-//~ error_log("Hash: $hash
-//~ Expected: $signature
-//~ Message: $doc
-//~ Input: $input");
-
-if( $hash !== $signature )
-{
-	$response['response'] = $hash;
-	header( $__http_status[$response['status']] );
-	die(json_encode($response));
-}
-
 /**
  * @see	\Service\Model::init()
  */
-$response = call_user_func_array( $page->callback, array(
+$page->response = call_user_func_array( $page->callback, array(
 	'method'	=>	$_SERVER['REQUEST_METHOD'],
 	'params'	=>	$page->params,
 	'data'		=>	$data
@@ -95,17 +31,16 @@ $response = call_user_func_array( $page->callback, array(
 // determine response type and pass response through to the page handler
 if( $page->content_type === 'application/json' )
 {
-	$page->body = json_encode( $response );
+	$page->response = json_encode( $response );
 }
 elseif( $page->content_type === 'application/xml' )
 {
 }
 else
 {
-	$response['status'] = $config->HTTP_NOT_ACCEPTABLE;
-	$response['message'] = 'The server was unable to understand your request,'
+	$page->response['status'] = $config->HTTP_NOT_ACCEPTABLE;
+	$page->response['message'] = 'The server was unable to understand your request,'
 		. ' please check your request parameters.';
-	$page->body = 'Invalid format';
 }
 
 header( $__http_status[$response['status']] );
@@ -117,5 +52,5 @@ if(	$config->DEV )
 	header( 'X-Execute-Time: ' .  $_finish__ - $_start__ );
 }
 
-$page->render( $page->body );
+$page->render( $page->response );
 ?>
