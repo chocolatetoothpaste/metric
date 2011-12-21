f9f7d0d24beb7f21a283956c8a229dd9bf0d35a0
Ross Paskett <rpaskett@gmail.com>
2011-12-21 00:07:52 -0700

    Working out some bugs from moving rest authentication into page controller.

--- rest.php
+++ rest.php
@@ -1,12 +1,12 @@
 <?php
 $page->template = false;
-$page->authorize();
-$page->content_type = $_SERVER['HTTP_ACCEPT'];
 $page->response = array(
 	'success' => 'false',
 	'status' => $config->HTTP_UNAUTHORIZED,
 	'message' => 'You do not have permission to access the resource at '
 		. $page->request );
+$page->authorize();
+$page->content_type = $_SERVER['HTTP_ACCEPT'];
 
 if( $_SERVER['REQUEST_METHOD'] === 'GET' )
 {
@@ -31,7 +31,7 @@ $page->response = call_user_func_array( $page->callback, array(
 // determine response type and pass response through to the page handler
 if( $page->content_type === 'application/json' )
 {
-	$page->response = json_encode( $response );
+	$page->body = json_encode( $page->response );
 }
 elseif( $page->content_type === 'application/xml' )
 {
@@ -43,7 +43,7 @@ else
 		. ' please check your request parameters.';
 }
 
-header( $__http_status[$response['status']] );
+header( $config->http_status[$page->response['status']] );
 header( 'Date: ' . gmdate( DATE_RFC1123 ) );
 
 if(	$config->DEV )
@@ -52,5 +52,5 @@ if(	$config->DEV )
 	header( 'X-Execute-Time: ' .  $_finish__ - $_start__ );
 }
 
-$page->render( $page->response );
+$page->render();
 ?>
