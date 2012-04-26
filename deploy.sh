5494e14b57483632891aa2b46d50df1fe578f7cf
Ross Paskett <ross@paskett.me>
2012-04-25 23:03:17 -0600

    Updated deply script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-CONFIG="<?php
+PAGE="<?php\n
+class Template extends \Metric\Page\Page {}\n
+?>";
 
-\$config->define( 'PATH_ROOT',		dirname( \$_SERVER['DOCUMENT_ROOT'] ) );
-\$config->define( 'PATH_LIB',		\$config->PATH_ROOT . '/${PWD##*/}' );
+CONFIG="<?php\n\n
+
+\$config->define( 'PATH_ROOT',		dirname( \$_SERVER['DOCUMENT_ROOT'] ) );\n
+\$config->define( 'PATH_LIB',		\$config->PATH_ROOT . '/${PWD##*/}' );\n
+\$config->define( 'PATH_PAGE',		\$config->PATH_ROOT . '/page');\n
+\$config->define( 'PATH_CONTROLLER',	\$config->PATH_PAGE . '/controller' );\n
+\$config->define( 'PATH_VIEW',		\$config->PATH_PAGE . '/view' );\n
+\$config->define( 'PATH_TEMPLATE',	\$config->PATH_PAGE . '/template' );\n\n
+\$config->classes = array(\n
+	'Template'	=>	\$config->PATH_TEMPLATE . '/template.class.php'\n
+);\n\n
+
+\$config->alias = array(\n
+	'/'	=>	\$config->PATH_CONTROLLER . '/index.php'\n
+);\n\n
+
+\$config->template = 'Template';\n
 ?>";
 
 HT="#RewriteEngine on
@@ -60,11 +77,15 @@ ErrorDocument 404 /404
 
 cd ..
 
-echo $CONFIG > config.inc.php
+echo -e $CONFIG > config.inc.php
 
 mkdir -p page/template
 mkdir page/controller
 mkdir page/view
+
+echo -e $PAGE > page/template/template.class.php
+touch page/controller/index.php
+echo 'The engine is running!' > page/view/index.phtml
 
 mkdir public
 echo "<?php include( '../lib/bootstrap.php' ); ?>" > public/index.php
