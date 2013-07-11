830f4ec05f3553a8a51f92c0bfe39a5d30c9c419
Ross Paskett <rpaskett@gmail.com>
2013-07-11 01:39:53 -0600

    Cleaning things up a big and improving some consistency
    Fixed some bugs in deploy script

--- bootstrap.php
+++ bootstrap.php
@@ -8,17 +8,18 @@ namespace Metric;
 // begin timing page execution
 $_start__ = microtime( true );
 
+// the order of the following 2 lines is important, don't change them
 include( 'include/include.inc.php' );
 session_start();
 
-$page = new $config->template();
-$page->parseURL( strtok( $_SERVER['REQUEST_URI'], '?' ) );
+$page = new Page();
 
 // make sure the page doesn't get cached unless told to
 header( "Cache-Control: must-revalidate, max-age=0" );
 
 // load the page/view then render it
-$page->load();
+$page->template( $config->template );
+$page->load( strtok( $_SERVER['REQUEST_URI'], '?' ) );
 $page->render();
 
 // end timing page execution and display it as a comment after </html>
